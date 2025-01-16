9f5ef698642eab8234813b7c453e56520c059de6
Grabelnikov R <grabelnikov.rus@mail.ru>
2025-01-16 18:32:34 +0700

    2-components: add LikeButton

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import { Card } from "@/components/Card/Card";
+"use client";
+import { Card, LikeButton } from "@/components";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
 
 const obj = {
   img: "/card-image.png",
@@ -10,6 +12,21 @@ const obj = {
 };
 
 export default function Home() {
+  const [isLike, setIsLike] = useState(false);
+
+  useEffect(() => {
+    async function setPosts() {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+        method: "PATCH",
+        body: JSON.stringify({ isLike }),
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      return res.json();
+    }
+    // корсы вызывают ошибку
+    setPosts().then(console.log);
+  }, [isLike]);
+
   return (
     <div className={styles.page}>
       <Card
@@ -20,6 +37,7 @@ export default function Home() {
         theme={obj.theme}
         term={obj.term}
       />
+      <LikeButton isLike={isLike} setIsLike={setIsLike} />
     </div>
   );
 }
--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export { Like } from "./Like/Like";
 export { Button } from "./Button/Button";
 export { Card } from "./Card/Card";
+export { LikeButton } from "./LikeButton/LikeButton";
